1e211ca6b5e8d32c588da01d8b90b90751efd858
Damon Huynh <dhuynh10072007@gmail.com>
2025-07-13 03:39:30 -0700

    Add starting position for snake; Make grid 17x17

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,18 +1,39 @@
 import Tile from "./Tile"
+import handleMovement from "../util/handleMovement";
+import { SnakeTile, Snake} from "./Snake";
 import styles from "../styles/Gameboard.module.css"
+import { useEffect, useState } from "react";
 
 function Gameboard(){
-    const gridSize = 256;
+    //17x17 grid
+    const gridSize = 289;
     const grid = new Array(gridSize);
     for (let i = 0; i < grid.length; i++){
         grid[i] = i;
     }
-    const tileGrid = grid.map((tile) => {
-        return <Tile key={tile}></Tile>
+
+    const [tileGrid, setTileGrid] = useState(()=> {
+        for (let i = 0; i < grid.length; i++){
+            grid[i] = <Tile key={i}></Tile>
+        }
+        const snakeStartingPosition = [138, 139, 140, 141];
+        snakeStartingPosition.forEach((index)=>{
+            grid[index] = <SnakeTile key={index}></SnakeTile>
+        });
+        return grid;
     });
+
+    useEffect(()=>{
+        window.addEventListener("keydown", handleMovement);
+    },[])
+
+
     return(
         <div className={styles.gameboard}>
             {tileGrid}
+            <Snake
+                setTileGrid={setTileGrid}
+            />
         </div>
     )
 }
