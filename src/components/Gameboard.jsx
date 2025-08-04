4f3e7b6940fe38e5b0532ee436a688ed2f37c3d8
Damon Huynh <dhuynh10072007@gmail.com>
2025-08-04 13:55:16 -0700

    change callback for movement

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,8 +1,9 @@
 import Tile from "./Tile"
-import handleMovement from "../util/handleMovement";
+// import handleMovement from "../util/handleMovement";
+import move from "../util/handleMovement";
 import { SnakeTile, Snake} from "./Snake";
 import styles from "../styles/Gameboard.module.css"
-import { useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 
 function Gameboard(){
     //17x17 grid
@@ -12,20 +13,27 @@ function Gameboard(){
         grid[i] = i;
     }
 
+    const [snakePosition, setSnakePosition] = useState([140,141])
     const [tileGrid, setTileGrid] = useState(()=> {
         for (let i = 0; i < grid.length; i++){
-            grid[i] = <Tile key={i}></Tile>
+            grid[i] = <Tile key={i} numberForDevPurposes={i}></Tile>
         }
-        const snakeStartingPosition = [138, 139, 140, 141];
-        snakeStartingPosition.forEach((index)=>{
-            grid[index] = <SnakeTile key={index}></SnakeTile>
-        });
         return grid;
     });
 
     useEffect(()=>{
-        window.addEventListener("keydown", handleMovement);
-    },[])
+        setTileGrid((prevGrid) => {
+            const newGrid = [...prevGrid];
+            snakePosition.forEach((index) => {
+                newGrid[index] = <SnakeTile key={index}></SnakeTile>
+            })
+            return newGrid;
+        })
+    }, [snakePosition]);
+
+    useEffect(()=>{
+        window.addEventListener("keydown", (e)=>move(e,setSnakePosition, setTileGrid));
+    },[]);
 
 
     return(
