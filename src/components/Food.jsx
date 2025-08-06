259ae312b88338216878d97c00133dd4936ec2dc
Damon Huynh <dhuynh10072007@gmail.com>
2025-08-06 11:42:17 -0700

    Create food component; add random spawning of food

---
+++ src/components/Food.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Food.module.css"
+
+
+
+function Food(){
+    return(
+        <div className={styles.food}></div>
+    )
+}
+
+export default Food;
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -4,19 +4,26 @@ import move from "../util/handleMovement";
 import { SnakeTile} from "./Snake";
 import styles from "../styles/Gameboard.module.css"
 import {useEffect, useState } from "react";
+import Food from "./Food";
 
 function Gameboard(){
     //17x17 grid
-    const gridSize = 289;
+    const gridDimensions = 17;
+    const gridSize = gridDimensions * gridDimensions;
     const grid = new Array(gridSize);
     for (let i = 0; i < grid.length; i++){
         grid[i] = i;
     }
 
     const [snakePosition, setSnakePosition] = useState([140,141,142,143])
+    const [foodPosition, setFoodPosition] = useState(147);
     const [tileGrid, setTileGrid] = useState(()=> {
         for (let i = 0; i < grid.length; i++){
-            grid[i] = <Tile key={i} numberForDevPurposes={i}></Tile>
+            if (i == foodPosition){
+                grid[i] = <Food key={i}></Food>;
+                continue;
+            }
+            grid[i] = <Tile key={i} numberForDevPurposes={i}></Tile>;
         }
         return grid;
     });
@@ -29,19 +36,46 @@ function Gameboard(){
             })
             return newGrid;
         })
-    }, [snakePosition]);
+    }, []);
+
+    const moveCallback = (e) => move(e,setSnakePosition, setTileGrid, gridDimensions);
 
     useEffect(()=>{
-        window.addEventListener("keydown", (e)=>move(e,setSnakePosition, setTileGrid));
+        window.addEventListener("keydown", moveCallback);
+        return () =>{
+            window.removeEventListener("keydown", moveCallback);
+        } 
     },[]);
+
+    useEffect(() => {
+        setTileGrid((prevGrid) => {
+            const newGrid = [...prevGrid];
+            newGrid[foodPosition] = <Food key={foodPosition}/>;
+            return newGrid;
+        })
+    }, [foodPosition])
+    
+    if (snakePosition[snakePosition.length - 1] === foodPosition){
+        setFoodPosition(()=>{
+            let counter = 0;
+            const allowedSpaces = new Array(gridSize - snakePosition.length);
+            for (let i = 0; i < gridSize; i++){
+                if (!snakePosition.includes(i)){
+                    allowedSpaces[i - counter] = i;
+                }
+                else{
+                    counter++;
+                }
+            }
+            let randomNumber = Math.floor(Math.random() * allowedSpaces.length);
+            return allowedSpaces[randomNumber];
+        })
+    }
 
 
     return(
         <div className={styles.gameboard}>
             {tileGrid}
-            <Snake
-                setTileGrid={setTileGrid}
-            />
         </div>
     )
 }
